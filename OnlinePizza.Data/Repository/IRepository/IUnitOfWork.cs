efb9b7c8ad9d153db1afb26e82a335c2341e5fea
ThaymesCoder <thamue1892@gmail.com>
2020-06-17 22:59:47 +0200

    Added IUnitOfWork interface

---
+++ OnlinePizza.Data/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlinePizza.Data.Repository.IRepository
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        void Save();
+    }
+}
